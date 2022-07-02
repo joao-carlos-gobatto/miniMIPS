60b42fc0100ff520d78257f667e9609a932a2885
João Carlos Gobatto da Cunha <joao.carlos@roxcode.dev>
2022-07-02 17:37:52 -0300

    Meio do processo

--- miTo.srcs/sources_1/new/miTo.vhd
+++ miTo.srcs/sources_1/new/miTo.vhd
@@ -11,22 +11,35 @@ use mito.mito_pkg.all;
 
 entity miTo is
   Port (
-    rst_n                       : in  std_logic;
-    clk                         : in  std_logic;
-    decoded_instruction         : out decoded_instruction_type;
-    instruction                 : out std_logic_vector (15 downto 0);
-    ram_addr                    : out std_logic_vector (7 downto 0); 
-    state_flag                  : out estados;
-    alu_op                      : out std_logic;
-    mem_write                   : out std_logic;
-    reg_write                   : out std_logic;
-    branch                      : out std_logic;
-    halt                        : out std_logic
+    clk             : in std_logic;
+    rst_n           : in std_logic;
+    data            : in std_logic_vector(15 downto 0);
+    addr_dest       : in std_logic_vector(1 downto 0);
+    addr_a          : in std_logic_vector(1 downto 0);
+    addr_b          : in std_logic_vector(1 downto 0);
+    data_to_mem     : out std_logic_vector(15 downto 0);
+    s0              : out std_logic_vector(15 downto 0);
+    s1              : out std_logic_vector(15 downto 0);
+    r0              : out std_logic_vector(15 downto 0);
+    r1              : out std_logic_vector(15 downto 0);
+    r2              : out std_logic_vector(15 downto 0);
+    r3              : out std_logic_vector(15 downto 0)
   );
 end miTo;
 
 architecture rtl of miTo is
 
+  data            : in std_logic_vector(15 downto 0);
+    addr_dest       : in std_logic_vector(1 downto 0);
+    addr_a          : in std_logic_vector(1 downto 0);
+    addr_b          : in std_logic_vector(1 downto 0);
+    data_to_mem     : out std_logic_vector(15 downto 0);
+    s0              : out std_logic_vector(15 downto 0);
+    s1              : out std_logic_vector(15 downto 0);
+    r0              : out std_logic_vector(15 downto 0);
+    r1              : out std_logic_vector(15 downto 0);
+    r2              : out std_logic_vector(15 downto 0);
+    r3              : out std_logic_vector(15 downto 0)
   signal decoded_instruction_s         : decoded_instruction_type;
   signal state_flag_s                  : estados;
   signal zero_flag_s                   : std_logic;
@@ -45,25 +58,6 @@ architecture rtl of miTo is
   signal mem_read_s                    : std_logic;
 begin
 
-control_unit_i : control_unit
-  Port map(
-    clk                        => clk,
-    rst_n                      => rst_n,
-    decoded_instruction        => decoded_instruction_s,
-    state_flag                 => state_flag_s,
-    reg_write                  => reg_write_s,
-    mem_write                  => mem_write_s,
-    mem_read                   => mem_read_s,
-    zero_flag                  => zero_flag_s,
-    alu_op                     => alu_op_s,
-    is_beq                     => is_beq_s,
-    is_load                    => is_load_s,
-    jump                       => jump_s,
-    branch                     => branch_s,
-    pc_enable                  => pc_enable_s,
-    halt                       => halt_s
-  );
-
 data_path_i : data_path
   Port map (
     clk                        => clk,
@@ -81,17 +75,6 @@ data_path_i : data_path
     reg_b_out                  => reg_b_out_s,
     ram_addr                   => ram_addr_s,
     reg_write                  => reg_write_s
-  );
-  
-memory_i : memory
-  Port map(
-    clk                        => clk,
-    rst_n                      => rst_n,
-    reg_b_out                  => reg_b_out_s,
-    mem_write                  => mem_write_s,
-	mem_read			       => mem_read_s,
-    ram_addr		           => ram_addr_s,
-    instruction                => instruction_s
   );
 
   alu_op                      <= alu_op_s;
--- miTo.srcs/sources_1/new/mito_pkg.vhd
+++ miTo.srcs/sources_1/new/mito_pkg.vhd
@@ -9,114 +9,58 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 
 package mito_pkg is
-  	type decoded_instruction_type is (
-		I_NOP,
-		I_ADD,
-		I_SUB,
-		I_LOAD,
-		I_STORE,
-		I_JUMP,
-		I_BEQ,
-		I_BNE,
-		I_HALT
-	);
-	type estados is (
-	    FETCH,
-	    NOP_E,
-	    PROX,
-        HALT_E,
-        ADD,
-        SUB,
-        LOAD,
-        LOAD1,
-        LOAD2,
-        LOAD3,
-        STORE,
-        JUMP_E,
-        BEQ,
-        BNE,
-        ULA,
-        BRANCH_E,
-        DECODE
-    );  
-  component data_path
+  	
+  component registers_bank
 	Port (
-		clk                		: in  std_logic;
-		rst_n              		: in  std_logic;
-		is_beq             		: in  std_logic;
-		jump               		: in  std_logic;
-		branch             		: in  std_logic;
-		pc_enable          		: in  std_logic;
-		halt               		: in  std_logic;
-		decoded_instruction		: out decoded_instruction_type;
-		alu_op             		: in  std_logic;
-		is_load           		: in  std_logic;
-		zero_flag          		: out std_logic;
-		reg_b_out               : out std_logic_vector(15 downto 0);
-		instruction           	: in  std_logic_vector (15 downto 0);
-		ram_addr              	: out std_logic_vector (7 downto 0);
-		reg_write				: in  std_logic
-	);
-  end component;
-
-  component control_unit
-    Port ( 
-  		clk                 	: in  std_logic;
-        rst_n               	: in  std_logic;
-        decoded_instruction  	: in  decoded_instruction_type;
-        state_flag              : out estados;
-        reg_write           	: out std_logic;
-        mem_write           	: out std_logic;
-        mem_read            	: out std_logic;
-        zero_flag           	: in  std_logic; 
-        alu_op              	: out std_logic;
-        is_beq              	: out std_logic;
-  		is_load					: out std_logic;
-        jump                	: out std_logic;
-        branch              	: out std_logic;
-        pc_enable           	: out std_logic;
-        halt                	: out std_logic
-  	);
-  end component;
-  
-  component memory is
-	Port (        
-		clk                 	: in  std_logic;
-        rst_n               	: in  std_logic;        
-        mem_write           	: in  std_logic;
-		mem_read				: in  std_logic;
-        ram_addr		    	: in  std_logic_vector(7  downto 0);
-		reg_b_out     			: in  std_logic_vector(15 downto 0);
-        instruction         	: out std_logic_vector(15 downto 0)
+		clk             : in std_logic;
+    	rst_n           : in std_logic;
+    	data            : in std_logic_vector(15 downto 0);
+    	addr_dest       : in std_logic_vector(1 downto 0);
+    	addr_a          : in std_logic_vector(1 downto 0);
+    	addr_b          : in std_logic_vector(1 downto 0);
+    	data_to_mem     : out std_logic_vector(15 downto 0);
+    	s0              : out std_logic_vector(15 downto 0);
+    	s1              : out std_logic_vector(15 downto 0);
+    	r0              : out std_logic_vector(15 downto 0);
+    	r1              : out std_logic_vector(15 downto 0);
+    	r2              : out std_logic_vector(15 downto 0);
+    	r3              : out std_logic_vector(15 downto 0) 
 	);
   end component;
 
   component mito
 	Port (
-		rst_n                   : in  std_logic;
-		clk                     : in  std_logic;
-		decoded_instruction		: out decoded_instruction_type;
-		state_flag              : out estados;
-		alu_op                  : out std_logic;
-		instruction             : out std_logic_vector (15 downto 0);
-		ram_addr                : out std_logic_vector (7 downto 0);
-		mem_write               : out std_logic;
-		reg_write               : out std_logic;
-		branch                  : out std_logic;
-		halt                    : out std_logic
+		clk             : in std_logic;
+    	rst_n           : in std_logic;
+    	data            : in std_logic_vector(15 downto 0);
+    	addr_dest       : in std_logic_vector(1 downto 0);
+    	addr_a          : in std_logic_vector(1 downto 0);
+    	addr_b          : in std_logic_vector(1 downto 0);
+    	data_to_mem     : out std_logic_vector(15 downto 0);
+    	s0              : out std_logic_vector(15 downto 0);
+    	s1              : out std_logic_vector(15 downto 0);
+    	r0              : out std_logic_vector(15 downto 0);
+    	r1              : out std_logic_vector(15 downto 0);
+    	r2              : out std_logic_vector(15 downto 0);
+    	r3              : out std_logic_vector(15 downto 0)
 	);
   end component;
   
  component testbench is
 	Port (
-		clk 					: in  std_logic := '0';
-		reset 					: in  std_logic;
-		decoded_instruction		: in  decoded_instruction_type;
-		state                   : in  estados;
-		alu_op                  : in  std_logic;
-		branch                  : in  std_logic;
-		instruction	 			: in  std_logic_vector (15 downto 0);
-		ram_addr		 		: in  std_logic_vector (15 downto 0)
+		clk             : out std_logic;
+    	rst_n           : out std_logic;
+    	data            : out std_logic_vector(15 downto 0);
+    	addr_dest       : out std_logic_vector(1 downto 0);
+    	addr_a          : out std_logic_vector(1 downto 0);
+    	addr_b          : out std_logic_vector(1 downto 0);
+    	data_to_mem     : in std_logic_vector(15 downto 0);
+    	s0              : in std_logic_vector(15 downto 0);
+    	s1              : in std_logic_vector(15 downto 0);
+    	r0              : in std_logic_vector(15 downto 0);
+    	r1              : in std_logic_vector(15 downto 0);
+    	r2              : in std_logic_vector(15 downto 0);
+    	r3              : in std_logic_vector(15 downto 0)
 	); 
   end component;
 
